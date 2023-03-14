821b13f9ab8f3293eac372c10a59b45e0ece015b
startym <183031250@qq.com>
2023-03-14 10:50:38 +0800

    新增变量

--- maven/src/main/java/com/cssl/App.java
+++ maven/src/main/java/com/cssl/App.java
@@ -5,12 +5,15 @@ public class App {
     private static int age = 10;
 
     private static int i = 100;
+	
+	private static int s;
 
     public static void main(String[] args) {
         String name = "admin";
         System.out.println("Hello:"+age);
         System.out.println("Hello Wolrd:"+i);
 		System.out.println("name:"+name);
+		System.out.println("s:"+s);
     }
 
 }

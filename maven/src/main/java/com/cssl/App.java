7866f29744b553ade407139966e80cd79d99c910
startym <183031250@qq.com>
2023-03-14 10:13:00 +0800

    修改app

--- maven/src/main/java/com/cssl/App.java
+++ maven/src/main/java/com/cssl/App.java
@@ -2,8 +2,12 @@ package com.cssl;
 
 public class App {
 
+    private int i = 100;
+
     public static void main(String[] args) {
-        System.out.println("Hello Wolrd!");
+		String name = "";
+        System.out.println("Hello Wolrd:"+i);
+		System.out.println("name:"+name);
     }
 
 }

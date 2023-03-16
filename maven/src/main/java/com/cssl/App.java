307f964bc9a58906f01faf5013fd0590e2e2d99c
startym <183031250@qq.com>
2023-03-16 09:39:04 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	maven/src/main/java/com/cssl/App.java

--- maven/src/main/java/com/cssl/App.java
+++ maven/src/main/java/com/cssl/App.java
@@ -1,14 +1,16 @@
 package com.cssl;
 
+import java.util.Date;
+
 public class App {
 
     private static int age = 10;
-
-
+    private static Date borndate;
 
     public static void main(String[] args) {
-        String name = "admin";
-        System.out.println("Hello:"+age);
+        System.out.println("date:"+borndate);
+        String name = "admin111";
+        System.out.println("Hello1:"+age);
         System.out.println("App");
     }
 
